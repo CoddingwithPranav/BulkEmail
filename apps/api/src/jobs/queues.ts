bbb3ca821a3cb689ab5fd85912dfc0e5015efbd4
Pranav Mishra <pranavmishra2101@gmail.com>
2025-11-25 23:26:42 +0545

    finished upload fature

--- apps/api/src/jobs/queues.ts
+++ apps/api/src/jobs/queues.ts
@@ -1,6 +1,12 @@
 import redis from "@repo/config/redis";
-import { Queue } from "bullmq";
+import { Queue, QueueEvents } from "bullmq";
 
+// Queue for adding jobs
 export const fileProcessingQueue = new Queue("file-processing", {
+  connection: redis,
+});
+
+// QueueEvents for listening to progress/completed/failed (shared globally)
+export const fileProcessingQueueEvents = new QueueEvents("file-processing", {
   connection: redis,
 });
--- apps/api/src/routes/v1/file.ts
+++ apps/api/src/routes/v1/file.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
 import {
+  deleteFile,
   downloadInvalidRows,
   getFileStatus,
+  getUserFiles,
   uploadFile,
 } from "../../controllers/fileController";
 import { protect } from "../../middleware/auth";
 import { upload } from "../../middleware/upload";
 
 const router: Router = Router();
-router.use(protect);
 
+router.use(protect);
 router.post("/upload", upload.single("file"), uploadFile);
+// router.get("/progress/:jobId", getFileProgressSSE);
+
+router.get("/list", getUserFiles);
 router.get("/status/:id", getFileStatus);
 router.get("/invalid/:id", downloadInvalidRows);
+router.delete("/:id", deleteFile);
 
 export default router;
